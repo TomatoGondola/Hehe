cd90320769b57c1e7562551af267d4cb357632e3
TomatoGondola <victorli163@gmail.com>
2024-09-09 12:27:05 +0800

    step 14: added paginator and fixed the page buttons

--- listings/views.py
+++ listings/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator 
 
 # Create your views here.
 
 def index(request):
-    # ? get all data from the listing database
-    listings = Listing.objects.all()
-    # ? pass database records into listings context
-    context = {'listings':listings}
+    # ! get all data from the listing database
+    listings = Listing.objects.all().order_by('list_date')
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    # ! pass database records into listings context
+    context = {'listings': paged_listings}
     #contexts = { 'name' : 'NEDM', 'community' : 'The dirtiest of dirties', }
     return render(request, "listings/listings.html", context,)
 
 def listing(request, listing_id):   
     return render(request, "listings/listing.html", )
-
 
 def search(request):
     return render(request, "listings/search.html")
