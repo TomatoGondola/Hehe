bd33f05a804dbe7c0b6da41a852499acb1df8ed4
TomatoGondola <victorli163@gmail.com>
2024-09-10 12:50:48 +0800

    step 16: choices, search, listing loop

---
+++ listings/choices.py
@@ -0,0 +1,46 @@
+bedroom_choices = {
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '4': '14',
+    '5': '5',
+    '6': '6',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+    '10': '10',
+}
+
+price_choices =  {
+    '1000000': '$1,000,000',
+    '2000000': '$2,000,000',
+    '3000000': '$3,000,000',
+    '4000000': '$,4000,000',
+    '5000000': '$5,000,000',
+    '6000000': '$6,000,000',
+    '7000000': '$7,000,000',
+    '8000000': '$8,000,000',
+    '9000000': '$9,000,000',
+    '10000000': '$10G+'
+}
+
+district_choices = {
+    'Islands': 'Islands',
+    'Kwai Tsing': 'Kwai Tsing',
+    'North': 'North',
+    'Sai Kung': 'Sai Kung',
+    'Kwai Tsing': 'Kwai Tsing',
+    'Yuen Long': 'Yuen Long',
+    'Kwun Tong': 'Kwun Tong',
+    'Sham Shui Poo': 'Sham Shui Poo',
+    'Wong Tai Sin': 'Wong Tai Sin',
+    'Tai Poo': 'Tai Poo',
+    'Sha Tin': 'Sha Tin',
+    'Sai Kung': 'Sai Kung',
+    'Yau Tsim Mong': 'Yau Tsim Mong',
+    'Kowloon City': 'Kowloon City',
+    'Eastern': 'Eastern',
+    'Central and Western': 'Central and Western',
+    'Southern': 'Southern',
+    'Wan Chai': 'Wan Chai'
+}
--- listings/views.py
+++ listings/views.py
@@ -1,23 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator 
 
 # Create your views here.
 
 def index(request):
-    # ! get all data from the listing database
+    # ? get all data from the listing database
     listings = Listing.objects.order_by('list_date').filter(is_published=True)
     paginator = Paginator(listings, 3)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
-    # ! pass database records into listings context
+    # ? pass database records into listings context
     context = {'listings': paged_listings}
     #contexts = { 'name' : 'NEDM', 'community' : 'The dirtiest of dirties', }
     return render(request, "listings/listings.html", context,)
 
-def listing(request, listing_id):   
-    return render(request, "listings/listing.html", )
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing,
+}   
+    return render(request, "listings/listing.html", context)
 
 def search(request):
     return render(request, "listings/search.html")
+    
+    
 
